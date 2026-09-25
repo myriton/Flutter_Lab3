82061f609f7ea3ab56417626739d5f97a5427a71
myriton <nikasevcenko642@gmail.com>
2026-09-25 14:10:52 +0300

    Add Scaffold, Center and disable debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: (Scaffold(
         body: Center(child: Text("Hello world!")),
       )),
